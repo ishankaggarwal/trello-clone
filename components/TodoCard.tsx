c152a6842679e23e45e3e1ca282fc5f0292b6e3a
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-26 17:15:44 -0800

    Added TodoCard functionality

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { XCircleIcon } from "@heroicons/react/16/solid";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -20,7 +23,21 @@ function TodoCard({
   dragHandleProps,
   draggableProps,
 }: Props) {
-  return <div></div>;
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex items-center justify-between p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoCard;
